778c9d1adb14fd8dd4f20494452309d7fe164e7f
ivanpozdin <ivanllpozdin@gmail.com>
2023-08-11 17:44:21 +0400

    Fix typo in getComputerChoice function
    From "moveID" which does not exist to moveIndex.

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ const moves = ["Rock", "Paper", "Scissors"];
 
 const getComputerChoice = function () {
   const moveIndex = Math.floor(Math.random() * 3);
-  return moves[moveID];
+  return moves[moveIndex];
 };
+
+console.log(getComputerChoice());
